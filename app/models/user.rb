c90bc219522e9cf3f65aba5ee2de8adc4b0e937d
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-07-06 14:46:44 -0400

    Initial commit
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :ldap_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
          
   has_and_belongs_to_many :projects
